62397735943907422a874ba0708039801a8f1fe6
probir ghosh <probirghosh@outlook.com>
2022-08-16 09:46:43 +0600

    js click event applied

---
+++ script.js
@@ -0,0 +1,22 @@
+//select dom element
+const counterEl = document.getElementById("counter-element");
+const incrementEl = document.getElementById("increment-element");
+const decrementEl = document.getElementById("decrement-element");
+
+//initial state
+let count = 0;
+
+// event listener
+incrementEl.addEventListener("click", () => {
+  // console.log("clicked");
+  count++;
+  // console.log(count)
+  counterEl.innerText = count;
+});
+
+decrementEl.addEventListener("click", () => {
+  // console.log("clicked");
+  count--;
+//   console.log(count);
+  counterEl.innerText=count;
+});
